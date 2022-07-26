e64bbe83aa7432b430450361aca794d963cf0635
Helio Miranda <h.miranda.developer@gmail.com>
2022-07-26 01:21:01 -0400

    Code cleanup part I

--- src/playground/assets/scripts/index.ts
+++
@@ -1,9 +0,0 @@
-import "@assets-styles/style.css";
-import { Engine } from "@engine-runtime/engine";
-
-// Custom Scripts Files - SECTION START
-// import "@assets-scripts/draft-testing-engine";
-import "@assets-scripts/main-scene-setup";
-// Custom Scripts Files - SECTION END
-
-Engine.GetInstance().Start();
